21e7f620156c0ed215f5dbb737b107b48c69aa57
Athiwat Suknok <athiwatsuknok@Athiwats-MacBook-Pro.local>
2023-08-31 20:15:26 +0700

    feat: add nav bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,9 @@
+// style={{ fontFamily: "Satoshi-Regular" }}
+import Homepage from "./pages/Homepage";
 const App = () => {
   return (
-    <div
-      className="bg-primaryOrange4"
-      style={{ fontFamily: "Satoshi-Regular" }}
-    >
-      <h1 className="text-6xl text-center">
-        Pet Sitter, <br />
-        Perfect <br />
-        For Your Pet
-      </h1>
+    <div className="">
+      <Homepage />
     </div>
   );
 };
