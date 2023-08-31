132979a6ac0e6db8fa62836ff88c732d6f9f5d08
ekkasit ponkhunthod <admin@admins-MacBook-Air.local>
2023-08-31 11:55:35 +0700

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <div
+      className="bg-primaryOrange4"
+      style={{ fontFamily: "Satoshi-Regular" }}
+    >
+      <h1 className="text-6xl text-center">
+        Pet Sitter, <br />
+        Perfect <br />
+        For Your Pet
+      </h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryGray1: "#3A3B46",
+        primaryGray2: "#5B5D6F",
+        primaryGray3: "#7B7E8F",
+        primaryGray4: "#AEB1C3",
+        primaryGray5: "#DCDFED",
+        primaryGray6: "#F6F6F9",
+        primaryOrange1: "#E44A0C",
+        primaryOrange2: "#FF7037",
+        primaryOrange3: "#FF986F",
+        primaryOrange4: "#FFB899",
+        primaryOrange5: "#FFD5C2",
+        primaryOrange6: "#FFF1EC",
+        secondaryYellow1: "#FFCA62",
+        secondaryYellow2: "#FFF5EC",
+        secondaryBlue1: "#76D0FC",
+        secondaryBlue2: "#ECFBFF",
+        secondaryGreen1: "#1CCD83",
+        secondaryGreen2: "#E7FDF4",
+        secondaryPink1: "#FABAC0",
+        secondaryPink2: "#FFF0F1",
+        secondaryEtc1: "#000000",
+        secondaryEtc2: "#F6F6F9",
+        secondaryEtc3: "#EA1010",
+      },
+    },
+  },
+  plugins: [],
+};
