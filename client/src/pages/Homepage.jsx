663201434f9a126d14b5acf6443f7cc96b110c82
Athiwat Suknok <athiwatsuknok@Athiwats-MacBook-Pro.local>
2023-09-01 14:23:57 +0700

    feat: add pet sitter slogan on page

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
+import PetCareSlogan from "../components/PetCareSlogan";
 
 const Homepage = () => {
   return (
     <div>
       <Header />
+      <PetCareSlogan />
     </div>
   );
 };
