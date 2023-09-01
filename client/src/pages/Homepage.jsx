27459ab4a52f1ae56fb3024bd22c8fde43fdb5fa
Athiwat Suknok <athiwatsuknok@Athiwats-MacBook-Pro.local>
2023-09-01 17:17:59 +0700

    feat: add our priority and testimonials

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import PetCareSlogan from "../components/PetCareSlogan";
+import PetPriority from "../components/PetPriority";
 
 const Homepage = () => {
   return (
     <div>
       <Header />
       <PetCareSlogan />
+      <PetPriority />
     </div>
   );
 };
