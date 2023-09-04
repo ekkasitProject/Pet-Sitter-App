5ad21af733345839fef5aec1a4a553401580777b
doyyl <63501344+doyyl@users.noreply.github.com>
2023-09-04 08:40:29 +0700

    feat:register form adding or continue with& alredy have an account feature complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,11 +7,11 @@ const App = () => {
       className="bg-primaryOrange4"
       style={{ fontFamily: "Satoshi-Regular" }}
     >
-      <h1 className="text-6xl text-center">
+      {/* <h1 className="text-6xl text-center">
         Pet Sitter, <br />
         Perfect <br />
         For Your Pet
-      </h1>
+      </h1> */}
       <RegistrationForm />
     </div>
   );
--- client/src/pages/Registerpage.jsx
+++ client/src/pages/Registerpage.jsx
@@ -21,41 +21,106 @@ function RegistrationForm() {
   };
 
   return (
-    <div>
-      <h1>Registration Page</h1>
-      <form onSubmit={handleSubmit}>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
         <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
+          <h1 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Join us!
+          </h1>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-500">
+            Find your perfect pet sitter with us
+          </h2>
         </div>
-        <div>
-          <label>Phone Number:</label>
-          <input
-            type="tel"
-            name="phoneNumber"
-            value={formData.phoneNumber}
-            onChange={handleChange}
-            required
-          />
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email:
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+              className="mt-1 p-2 block w-full rounded-md border border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="phoneNumber"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Phone Number:
+            </label>
+            <input
+              id="phoneNumber"
+              name="phoneNumber"
+              type="tel"
+              value={formData.phoneNumber}
+              onChange={handleChange}
+              required
+              className="mt-1 p-2 block w-full rounded-md border border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password:
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+              className="mt-1 p-2 block w-full rounded-md border border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div className="mt-6">
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Register
+            </button>
+          </div>
+        </form>
+        <div className="mt-6">
+          <p className="text-center text-sm text-gray-600">Or continue with</p>
+          <div className="flex justify-center mt-2">
+            <button
+              type="button"
+              className="group relative w-full flex justify-center py-2 px-4 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            >
+              Facebook
+            </button>
+            <button
+              type="button"
+              className="ml-3 group relative w-full flex justify-center py-2 px-4 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            >
+              Gmail
+            </button>
+          </div>
         </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
+        <div className="mt-6">
+          <p className="text-center text-sm text-gray-600">
+            Already have an account?{" "}
+            <a
+              href="/login"
+              className="font-medium text-indigo-600 hover:text-indigo-500"
+            >
+              Login
+            </a>
+          </p>
         </div>
-        <button type="submit">Register</button>
-      </form>
+      </div>
     </div>
   );
 }
