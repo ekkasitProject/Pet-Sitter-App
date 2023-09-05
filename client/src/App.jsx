26a99c83a53ce3c64c61ceb7527e9a835575525c
Athiwat Suknok <athiwatsuknok@Athiwats-MacBook-Pro.local>
2023-09-05 13:53:08 +0700

    fix: commit file change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import RegistrationForm from "./pages/Registerpage";
 import Homepage from "./pages/Homepage";
+import LoginPage from "./pages/LoginPage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
@@ -9,7 +10,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/register" element={<RegistrationForm />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
 };
+
+export default App;
