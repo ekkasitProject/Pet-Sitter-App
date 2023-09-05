dbbb8c7e10aa4f28b6ddcddee14080bd687f04f4
Athiwat Suknok <athiwatsuknok@Athiwats-MacBook-Pro.local>
2023-09-05 18:12:32 +0700

    feat: add navigate to login

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import logo from "../assets/images/elements/logo.svg";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/login");
+  };
+
   return (
     <header className="overflow-hidden">
       <nav className="flex justify-between items-center w-[80%] mx-auto h-[80px] ">
@@ -9,7 +16,9 @@ const Header = () => {
           <img src={logo} alt="logo" />
         </div>
         <div>
-          <button className="mr-6">Login</button>
+          <button className="mr-6" onClick={handleClick}>
+            Login
+          </button>
           <button className="bg-[#FF7037] px-3 py-2 rounded-full text-white font-medium">
             Find A Pet Sitter
           </button>
