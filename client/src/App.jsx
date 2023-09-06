f06da2ed5974f459cff3bf11cdf9a0001c50b144
Athiwat Suknok <athiwatsuknok@Athiwats-MacBook-Pro.local>
2023-09-06 19:51:02 +0700

    fix: resovle  authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,11 @@
 import React from "react";
-import "./App.css";
-import RegistrationForm from "./pages/Registerpage";
-import Homepage from "./pages/Homepage";
-import LoginPage from "./pages/LoginPage";
-import PetSitterDetail from "./pages/PetSitterDetail";
-import PetSitterList from "./pages/PetSitterList";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { ThemeProvider } from "@mui/material/styles";
-import theme from "./theme.js";
-import { AuthProvider } from "./context/authentication";
+import AuthenticatedApp from "./pages/AuthenticatedApp";
+import UnauthenticatedApp from "./pages/UnauthenticatedApp";
+import { useAuth } from "./context/authentication";
 
-const App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <Router>
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/register" element={<RegistrationForm />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/petsitterlist" element={<PetSitterList />} />
-          <Route path="/petsister_id" element={<PetSitterDetail />} />
-        </Routes>
-      </AuthProvider>
-    </Router>
-</ThemeProvider>
-
-  );
-};
+function App() {
+  const { isAuthenticated } = useAuth();
+  return isAuthenticated ? <AuthenticatedApp /> : <UnauthenticatedApp />;
+}
 
 export default App;
---
+++ client/src/pages/AuthenticatedApp.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import RegistrationForm from "./Registerpage";
+import Homepage from "./Homepage";
+import LoginPage from "./LoginPage";
+import PetSitterList from "./PetSitterList";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../theme.js";
+import PetSitterDetail from "./PetSitterDetail";
+
+const AuthenticatedApp = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/register" element={<RegistrationForm />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/petsitterlist" element={<PetSitterList />} />
+        <Route path="/petsister_id" element={<PetSitterDetail />} />
+      </Routes>
+    </ThemeProvider>
+  );
+};
+
+export default AuthenticatedApp;
