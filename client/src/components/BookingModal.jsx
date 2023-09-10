a9df42a57030cc2f98085a8d57a6c93fad91a97c
GivPenjinun <penjinun.watt@gmail.com>
2023-09-10 20:40:42 +0700

    feat: add content to Booking History Card

--- client/src/components/BookingModal.jsx
+++ client/src/components/BookingModal.jsx
@@ -11,23 +11,45 @@ export default function BookingModal() {
   return (
     <>
       <div className="modal font-satoshi bg-neutral-700/80 w-screen h-screen z-10 top-0 left-0 right-0 bottom-0 fixed flex justify-center items-center">
-        <div className="bg-white shadow-custom3 w-[350px] h-[200px] rounded-xl flex flex-col">
-          <div className="text-headLine5 py-3 flex justify-between px-5">
+        <div className="bg-white shadow-custom3 w-[550px] h-[470px] rounded-xl flex flex-col justify-between">
+          <div className="text-headLine4 py-4 flex justify-between px-8">
             Booking Detail
             <button onClick={toggleBookingModal}>x</button>
           </div>
           <hr />
 
-          <div className="px-5 py-5 text-body3 text-primaryGray3">
-            Waiting for confirm
+          <div className="px-5 py-5 text-body3 text-primaryGray3 flex flex-col justify-between gap-3">
+            <div className="flex-1">Waiting for confirm</div>
+            <div className="flex-1">
+              <p>Transaction Date</p>
+              <p>Transaction No</p>
+            </div>
+            <div className="flex-1">
+              <div className="text-primaryGray3">Pet Sitter:</div>
+              <div className="text-black">Pet Sitter Name</div>
+            </div>
+            <div className="flex-1 flex flex-row">
+              <div className="flex-1">
+                <div className="text-primaryGray3">Date & Time:</div>
+                <div className="text-black">|</div>
+              </div>
+              <div className="flex-1">
+                <div className="text-primaryGray3">Duration:</div>
+                <div className="text-black">3 hours</div>
+              </div>
+            </div>
+            <div className="flex-1">
+              <div className="text-primaryGray3">Pet:</div>
+              <div className="text-black">Name</div>
+            </div>
           </div>
-          <div className="px-5 w-full h-full flex justify-between items-start">
-            <div onClick={toggleBookingModal}>
-              <Button1 button="Cancel" />
-            </div>
-            <div onClick="">
-              <Button2 button="Delete" />
-            </div>
+          <div className="flex justify-center">
+            <hr className="w-11/12 " />
+          </div>
+
+          <div className="px-5 mb-7 pt-2 w-full flex justify-between items-start">
+            <h1>Total</h1>
+            <h1>900 THB</h1>
           </div>
         </div>
       </div>
