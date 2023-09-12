c949af744e1f6bf7bfd3894502b653a25e508fcb
GivPenjinun <penjinun.watt@gmail.com>
2023-09-12 12:51:51 +0700

    fix: dateOfBirth bug

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -24,13 +24,28 @@ function UserProfile() {
     console.log(petOwnerProfile);
   }, []);
 
+  function formatDateToYYYYMMDD(isoDate) {
+    const date = new Date(isoDate);
+
+    // Extract year, month, and day components
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // Adding 1 to month (zero-indexed) and zero-padding
+    const day = String(date.getDate()).padStart(2, "0"); // Zero-padding day
+
+    // Construct the formatted date string
+    const formattedDate = `${year}-${month}-${day}`;
+
+    return formattedDate;
+  }
+
   useEffect(() => {
     if (petOwnerProfile) {
       setUsername(petOwnerProfile.username);
       setEmail(petOwnerProfile.email);
       setPhone(petOwnerProfile.phone);
       setIDNumber(petOwnerProfile.id_card_number);
-      setDateOfBirth(petOwnerProfile.date_of_birth);
+      const newDate = formatDateToYYYYMMDD(petOwnerProfile.date_of_birth);
+      setDateOfBirth(newDate);
     }
   }, [petOwnerProfile]);
 
