d863b11f3134e6bac8435ee3ceb7d9951940ac8d
GivPenjinun <penjinun.watt@gmail.com>
2023-09-19 16:12:37 +0700

    feat: add getBookingByID API

--- client/src/pages/BookingHistory.jsx
+++ client/src/pages/BookingHistory.jsx
@@ -32,27 +32,6 @@ function BookingHistory() {
     console.log(bookingHistory);
   }, []);
 
-  const changeDate = (date) => {
-    const newDate = new Date(date).toLocaleDateString();
-    const newTime = new Date(date).toLocaleTimeString();
-    return `${newDate} | ${newTime}`;
-  };
-
-  const changeTime = (date) => {
-    const newDate = new Date(date).toLocaleDateString();
-    return `${newDate}`;
-  };
-
-  function calculateDuration(startTime, endTime) {
-    const startDate = new Date(startTime);
-    const endDate = new Date(endTime);
-    const timeDifference = endDate - startDate;
-    const hours = Math.floor(timeDifference / 3600000);
-    //   const minutes = Math.floor((timeDifference % 3600000) / 60000);
-    // return `${hours} hours: ${minutes} minutes`;
-    return `${hours} hours`;
-  }
-
   return (
     <>
       <HeaderAuth />
