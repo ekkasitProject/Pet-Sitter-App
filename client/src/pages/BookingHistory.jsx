a17509d1ba37c63637b28924ee9a6ee2c7492b74
GivPenjinun <penjinun.watt@gmail.com>
2023-09-19 15:44:13 +0700

    feat: add booking card

--- client/src/pages/BookingHistory.jsx
+++ client/src/pages/BookingHistory.jsx
@@ -5,6 +5,7 @@ import SideBar from "../components/SideBar";
 import HeaderAuth from "../components/HeaderAuth";
 import BookingModal from "../components/BookingModal";
 import fetchUserData from "../hooks/fetchUserData";
+import { PhoneIcon } from "../components/Icons";
 
 function BookingHistory() {
   const { getBooking, bookingHistory, setBookingHistory, isError, isLoading } =
@@ -26,6 +27,27 @@ function BookingHistory() {
     console.log(bookingHistory);
   }, []);
 
+  const changeDate = (date) => {
+    const newDate = new Date(date).toLocaleDateString();
+    const newTime = new Date(date).toLocaleTimeString();
+    return `${newDate} | ${newTime}`;
+  };
+
+  const changeTime = (date) => {
+    const newDate = new Date(date).toLocaleDateString();
+    return `${newDate}`;
+  };
+
+  function calculateDuration(startTime, endTime) {
+    const startDate = new Date(startTime);
+    const endDate = new Date(endTime);
+    const timeDifference = endDate - startDate;
+    const hours = Math.floor(timeDifference / 3600000);
+    //   const minutes = Math.floor((timeDifference % 3600000) / 60000);
+    // return `${hours} hours: ${minutes} minutes`;
+    return `${hours} hours`;
+  }
+
   return (
     <>
       <HeaderAuth />
@@ -37,46 +59,104 @@ function BookingHistory() {
           <div className="booking-wrapper">
             {isError ? <h1>Request failed</h1> : null}
             {isLoading ? <h1>Loading ....</h1> : null}
-            <div
-              onClick={toggleBookingModal}
-              //  onClick={() => handleToggleBookingModal(booking.booking_id)}
-              className="booking-card border-2 border-primaryGray5 w-full h-2/12 my-5 p-5 rounded-2xl flex flex-col cursor-pointer hover:border-primaryOrange4"
-            >
-              <div className="flex flex-row gap-5 p-2">
-                <img
-                  src={profile_user}
-                  className="rounded-full w-[60px] h-[60px]"
-                  alt="pet sitter profile picture"
-                />
-                <div className="flex flex-col justify-center w-2/3 h-full">
-                  <h1 className="text-headLine3">petSitterName</h1>
-                  <h1 className="text-body1">By username</h1>
+            {/*mapping */}
+            {bookingHistory.map((booking) => {
+              return (
+                <div
+                  key={booking.booking_id}
+                  onClick={() => handleToggleBookingModal(booking.booking_id)}
+                  className={
+                    booking.status_booking == "In service"
+                      ? "booking-card border-2 w-full h-2/12 my-5 p-5 rounded-2xl flex flex-col cursor-pointer border-secondaryBlue1 hover:border-primaryOrange4"
+                      : "booking-card border-2 border-primaryGray5 w-full h-2/12 my-5 p-5 rounded-2xl flex flex-col cursor-pointer hover:border-primaryOrange4"
+                  }
+                >
+                  <div className="flex flex-row gap-5 p-2">
+                    <img
+                      src={profile_user}
+                      className="rounded-full w-[60px] h-[60px]"
+                      alt="pet sitter profile picture"
+                    />
+                    <div className="flex flex-col justify-center w-2/3 h-full">
+                      <h1 className="text-headLine3">
+                        {booking.petSitter.petSisterDetail[0].pet_sister_name}
+                      </h1>
+                      <h1 className="text-body1">
+                        By {booking.petSitter.username}
+                      </h1>
+                    </div>
+                    <div className="w-full h-full flex flex-col justify-center items-end pr-5">
+                      <div className="text-primaryGray3">
+                        {booking.status_booking == "Success" ? (
+                          <p>Booking date: {changeTime(booking.datetime)}</p>
+                        ) : booking.status_booking == "In service" ? (
+                          <p>Booking date: {changeTime(booking.datetime)}</p>
+                        ) : booking.status_booking == "Waiting for confirm" ? (
+                          <p>
+                            Transaction date: {changeTime(booking.datetime)}
+                          </p>
+                        ) : null}
+                      </div>
+                      <div
+                        className={
+                          booking.status_booking == "Success"
+                            ? "text-secondaryGreen1"
+                            : booking.status_booking == "In service"
+                            ? "text-secondaryBlue1"
+                            : booking.status_booking == "Waiting for confirm"
+                            ? "text-secondaryPink1"
+                            : null
+                        }
+                      >
+                        {booking.status_booking}
+                      </div>
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="date-section p-3 py-5 flex justify-between">
+                    <div className="flex-1">
+                      <div className="text-primaryGray3">Date&Time:</div>
+                      <div className="">
+                        <p>Start: {changeDate(booking.startTime)}</p>
+                        <p>End: {changeDate(booking.endTime)}</p>
+                      </div>
+                    </div>
+                    <div className="border-l-2 pl-8 basis-1/4">
+                      <div className="text-primaryGray3">Duration:</div>
+                      <div className="">
+                        {calculateDuration(booking.startTime, booking.endTime)}
+                      </div>
+                    </div>
+                    <div className="border-l-2 pl-8 basis-1/4">
+                      <div className="text-primaryGray3">Pet:</div>
+                      <div className="">Name</div>
+                    </div>
+                  </div>
+                  <div
+                    className={
+                      booking.status_booking == "Success"
+                        ? "status-section text-secondaryGreen1 w-full h-[80px] bg-secondaryGreen2 rounded-lg px-5 flex justify-between items-center"
+                        : "status-section text-primaryGray3 w-full h-[80px] bg-primaryGray6 rounded-lg px-5 flex justify-between items-center"
+                    }
+                  >
+                    {booking.status_booking == "Success" ? (
+                      <p>Success date: {changeDate(booking.endTime)}</p>
+                    ) : booking.status_booking == "In service" ? (
+                      <p>Your pet is already in Pet Sitter care!</p>
+                    ) : booking.status_booking == "Waiting for confirm" ? (
+                      <p>Waiting Pet Sitter for confirm booking </p>
+                    ) : null}
+                    <span>
+                      {booking.status_booking == "In service" && (
+                        <div className="w-[50px] h-[50px] rounded-full bg-primaryOrange6 text-primaryOrange2 flex justify-center items-center">
+                          <PhoneIcon />
+                        </div>
+                      )}
+                    </span>
+                  </div>
                 </div>
-                <div className="w-full h-full flex flex-col justify-center items-end pr-5">
-                  <div className="text-primaryGray3">transaction</div>
-                  <div className="text-secondaryGreen1 ">status</div>
-                </div>
-              </div>
-              <hr />
-              <div className="date-section p-3 py-5 flex justify-between">
-                <div className="flex-1">
-                  <div className="text-primaryGray3">Date&Time:</div>
-                  <div className="">|</div>
-                </div>
-                <div className="border-l-2 pl-8 flex-1">
-                  <div className="text-primaryGray3">Duration:</div>
-                  <div className="">3 hours</div>
-                </div>
-                <div className="border-l-2 pl-8 flex-1">
-                  <div className="text-primaryGray3">Pet:</div>
-                  <div className="">Name</div>
-                </div>
-              </div>
-              <div className="status-section text-primaryGray3 w-full h-[80px] bg-primaryGray6 rounded-lg px-5 flex justify-between items-center">
-                message
-                <span>x</span>
-              </div>
-            </div>
+              );
+            })}
           </div>
         </div>
       </div>
